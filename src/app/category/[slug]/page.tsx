68edd694bffd89184dba4ed396c5d3bf33c99fec
midhuna-21 <krishnamidhuna850@gmail.com>
2025-07-08 12:55:34 +0530

    slug issue udpating01

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,25 +1,22 @@
+import { notFound } from 'next/navigation';
 import { navData, slugToKeyMap } from '@/data/navData';
 import CategoryContent from '@/components/CategoryContent';
 import NavigationSection from '@/components/NavigationSection';
-import { notFound } from 'next/navigation';
 
-type Props = {
-  params: {
-    slug: string;
-  };
-};
-
-export default async function CategoryPage({ params }: Props) {
+export default async function CategoryPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   const { slug } = params;
 
   const navKey = slugToKeyMap[slug];
-
   if (!navKey || !navData[navKey]) return notFound();
 
   return (
     <>
       <NavigationSection />
-      <div className='container-fluid'>
+      <div className="container-fluid">
         <CategoryContent activeMain={navKey} />
       </div>
     </>
